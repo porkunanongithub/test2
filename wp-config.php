e50668670297d382ebca21682e493983222d648b
kunanongithub <porkunanon@gmail.com>
2018-07-20 15:48:48 +0700

    fix

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wp_ecommerce');
+define('DB_NAME', 'sql12248006');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'sql12248006');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'cm7wx867Ww');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'sql12.freemysqlhosting.net');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
